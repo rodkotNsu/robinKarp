c7baa425ce18005024e16558395e673cba83609e
Родион Котов <rodtdt@yandex.ru>
2020-11-12 16:29:42 +0700

    main

--- robinKarp/rb.c
+++ robinKarp/rb.c
@@ -4,7 +4,7 @@
 #include<malloc.h>
 #define SIZE_OF_BLOCK 150
 
-typedef   unsigned long long big_int;
+typedef unsigned long long big_int;
 big_int  arr_pow_3[SIZE_OF_BLOCK];
 
 big_int init_hash(char* str, int begin, int len_temp) {
@@ -15,27 +15,23 @@ big_int init_hash(char* str, int begin, int len_temp) {
 	}
 	return result;
 }
-int recount_hash(big_int* hash, int  length_temp, char first_c, char last_c) {
+void recount_hash(big_int* hash, int  length_temp, char first_c, char last_c) {
 	*hash -= ((unsigned char)first_c % 3);
 	*hash /= 3;
 	*hash += (((unsigned char)last_c % 3) * (arr_pow_3[length_temp - 1]));
-	return 0;
 }
-int init_array_pow_3(int n) {
+void init_array_pow_3(int n) {
 	big_int result = 1;
-	//  big_int * arr = (big_int*)malloc((n+1) * sizeof(big_int));
 	for (int i = 0; i < n + 1; i++) {
 		arr_pow_3[i] = result;
 		result *= 3;
 	}
-	return 0;
 }
-int re_init_array(char* line, int len, int number) {
-	for (int i = 0; i < len - number; i++) 
+void  re_init_array(char* line, int len, int number) {
+	for (int i = 0; i < len - number; i++)
 		line[i] = line[number + i];
-	return 0;
 }
-int robin_karp(FILE* file, char* temp) {
+void robin_karp(FILE* file, char* temp) {
 	big_int hash_s, hash_t;
 
 	char line[SIZE_OF_BLOCK] = { 0 };
@@ -47,19 +43,20 @@ int robin_karp(FILE* file, char* temp) {
 	char past_firs_char;
 	init_array_pow_3(len_temp);
 	hash_t = init_hash(temp, 0, len_temp);
-	
+
 	printf("%llu ", hash_t);
-	
+
 	while (!feof(file)) {
+		//последовательное считывание строки начиная с последнего элемента,который храниться в буфере
 		size_t len_read = fread(&line[start_write_with], sizeof(char), SIZE_OF_BLOCK - start_write_with, file);
 		if (len_read + start_write_with < len_temp)
 			break;
 
-		if (start_write_with != 0) 
+		if (start_write_with != 0)
 			recount_hash(&hash_s, len_temp, past_firs_char, line[len_temp - 1]);
-		else  
+		else
 			hash_s = init_hash(line, 0, len_temp);
-		 
+
 		for (size_t i = 0; i <= len_read + start_write_with - len_temp; i++) {
 			if (i != 0)
 				recount_hash(&hash_s, len_temp, line[i - 1], line[i + len_temp - 1]);
@@ -80,15 +77,14 @@ int robin_karp(FILE* file, char* temp) {
 		start_write_with = len_temp - 1;
 		number += (SIZE_OF_BLOCK - len_temp + 1);
 	}
-	return 0;
 }
 int main()
-{	char temp[SIZE_OF_BLOCK] = { 0 };
-	//FILE* file = fopen("C:\\Users\\anas2\\source\\repos\\robinKarp\\Debug\\in.txt", "r");
-	FILE* file = stdin;
-	if (!fgets(temp, SIZE_OF_BLOCK, file))
+{
+	char temp[SIZE_OF_BLOCK] = { 0 };
+	 //считаывание шаблона
+	if (!fgets(temp, SIZE_OF_BLOCK, stdin))
 		return 1;
-	robin_karp(file, temp);
-	fclose(file);
+	robin_karp(stdin, temp);
+	fclose(stdin);
 	return 0;
 }
